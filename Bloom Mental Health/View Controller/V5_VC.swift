49827563230f48cfe1c190141f4d54dd5e9d3a02
Jonathan Chi <chijonathan593@gmail.com>
2023-09-15 00:39:42 -0400

    V5 updates

--- Bloom Mental Health/View Controller/V5_VC.swift
+++ Bloom Mental Health/View Controller/V5_VC.swift
@@ -32,10 +32,12 @@ class V5_VC : UIViewController {
         lb.frame = CGRect(x: 0, y: 0, width: 100, height: 25)
         lb.text = "Emergency Contacts"
         lb.textAlignment = .left
-        lb.font = UIFont(name: "EpilogueRoman", size: 22)
+        lb.font = UIFont(name: "EpilogueRoman-Bold", size: 18)
         lb.textColor = .black
         return lb
     }()
+    
+    var array_for_button = ["Neil", "Jenny", "Add"]
     
     public func setup_UI(){
         let x = view.frame.width
@@ -53,7 +55,50 @@ class V5_VC : UIViewController {
         view.addSubview(emergency_contact)
         emergency_contact.frame = CGRect(x: 40, y: container.frame.maxY+20, width: emergency_contact.intrinsicContentSize.width, height: emergency_contact.intrinsicContentSize.height+10)
         
+        let x_padding = 30
+        let y_padding = 15
         
+        let width = 90
+        
+        for (index, item) in array_for_button.enumerated() {
+            let btn = UIButton()
+            btn.frame = CGRect(x: 0, y: 0, width: width, height: width)
+            btn.setTitle(array_for_button[index], for: .normal)
+            btn.backgroundColor = .purple
+            btn.layer.cornerRadius = CGFloat(width/2)
+            btn.titleLabel!.font = Style.myApp.font(for: .subtitle)
+            btn.setTitleColor(.white, for: .normal)
+            
+            view.addSubview(btn)
+            //handles circle buttons
+            if index == 0 {
+                btn.frame = CGRect(x: x_padding, y: Int(emergency_contact.frame.maxY) + y_padding, width: width, height: width)
+            }
+            
+            btn.frame = CGRect(x: x_padding * (index+1) + width * index, y: Int(emergency_contact.frame.maxY) + y_padding, width: width, height: width)
+            
+            let lb = UILabel()//handles labels
+            
+            lb.text = array_for_button[index]
+            lb.frame = CGRect(x: 0, y: 0, width: lb.intrinsicContentSize.width, height: lb.intrinsicContentSize.height)
+            lb.textAlignment = .center
+            view.addSubview(lb)
+            lb.center = CGPoint(x: btn.frame.midX, y: btn.frame.maxY + 20)
+            
+            
+            if index != array_for_button.count - 1 {
+                let btn = UIButton()
+                btn.setTitle("Edit Contact", for: .normal)
+                btn.titleLabel!.font = Style.myApp.font(for: .information)
+                btn.setTitleColor(.systemPurple, for: .normal)
+                
+                view.addSubview(btn)
+                btn.frame = CGRect(x: 0, y: 0, width: btn.intrinsicContentSize.width, height: btn.intrinsicContentSize.height)
+                btn.center = CGPoint(x: lb.frame.midX, y: lb.frame.maxY + 10)
+                
+            }
+            
+        }
     }
     
     
